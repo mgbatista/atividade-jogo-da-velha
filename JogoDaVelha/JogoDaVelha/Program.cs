c6ff1ebb203fd4c519bd2a28a40d70eaf9f1b4ef
Maiara Gabi Batista <maiara.27bueno@gmail.com>
2021-02-13 09:22:36 -0300

    Criando função verifica status

--- JogoDaVelha/JogoDaVelha/Program.cs
+++ JogoDaVelha/JogoDaVelha/Program.cs
@@ -13,6 +13,7 @@ namespace JogoDaVelha
             char[,] tabuleiro = new char[3, 3];
             int linhaj1, linhaj2, colunaj1, colunaj2;
             bool podecolocarpeca = false;
+            char resultadodojogo = '0';
 
             for (int i = 0; i < 3; i++)
             {
@@ -21,8 +22,9 @@ namespace JogoDaVelha
                     tabuleiro[i, j] = '-';
                 }
             }
+            Console.WriteLine("");
 
-            //do
+            do
             {
                 do
                 {
@@ -36,6 +38,7 @@ namespace JogoDaVelha
                 {
                     tabuleiro[linhaj1, colunaj1] = 'X';
                     imprimir_Jogo(tabuleiro);
+                    resultadodojogo = verifica_Status(tabuleiro);
                     // funções que testam e imprimem o jogo
                 }
 
@@ -51,11 +54,19 @@ namespace JogoDaVelha
                 {
                     tabuleiro[linhaj2, colunaj2] = 'O';
                     imprimir_Jogo(tabuleiro);
+                    resultadodojogo = verifica_Status(tabuleiro);
                     // funções que testam e imprimem o jogo
                 }
+            } while (verifica_Status(tabuleiro) == '0'); //jogo finalizado
 
-            } //while (jogofinalizado == true) //jogo finalizado
-
+            if (resultadodojogo == '1')
+            {
+                Console.WriteLine("O jogador 1 venceu!");
+            }
+            else
+            {
+                Console.WriteLine("O jogador 2 venceu!");
+            }
             Console.ReadKey();
 
         }
@@ -80,6 +91,77 @@ namespace JogoDaVelha
             {
                 Console.WriteLine("Posição indisponível, escolha outra coordenada!!!\n");
                 return (false);
+            }
+        }
+        static char verifica_Status(char[,] matriz)
+        {
+            if (matriz[0, 0] == 'X' && matriz[0, 0] == matriz[0, 1] && matriz[0, 0] == matriz[0, 2]) //
+            {
+                return ('1');
+            }
+            else if (matriz[0, 0] == 'X' && matriz[0, 0] == matriz[1, 0] && matriz[0, 0] == matriz[2, 0]) //
+            {
+                return ('1');
+            }
+            else if (matriz[0, 0] == 'X' && matriz[0, 0] == matriz[1, 1] && matriz[0, 0] == matriz[2, 2])
+            {
+                return ('1');
+            }
+            else if (matriz[1, 0] == 'X' && matriz[1, 0] == matriz[1, 1] && matriz[1, 0] == matriz[1, 2])
+            {
+                return ('1');
+            }
+            else if (matriz[2, 0] == 'X' && matriz[2, 0] == matriz[2, 1] && matriz[2, 0] == matriz[2, 2])
+            {
+                return ('1');
+            }
+            else if (matriz[2, 0] == 'X' && matriz[2, 0] == matriz[1, 1] && matriz[2, 0] == matriz[0, 2])
+            {
+                return ('1');
+            }
+            else if (matriz[0, 1] == 'X' && matriz[0, 1] == matriz[1, 1] && matriz[0, 1] == matriz[2, 1])
+            {
+                return ('1');
+            }
+            else if (matriz[0, 2] == 'X' && matriz[0, 2] == matriz[1, 2] && matriz[0, 2] == matriz[2, 2])
+            {
+                return ('1');
+            }
+            else if (matriz[0, 0] == 'O' && matriz[0, 0] == matriz[0, 1] && matriz[0, 0] == matriz[0, 2])
+            {
+                return ('2');
+            }
+            else if (matriz[0, 0] == 'O' && matriz[0, 0] == matriz[1, 0] && matriz[0, 0] == matriz[2, 0])
+            {
+                return ('2');
+            }
+            else if (matriz[0, 0] == 'O' && matriz[0, 0] == matriz[1, 1] && matriz[0, 0] == matriz[2, 2])
+            {
+                return ('2');
+            }
+            else if (matriz[1, 0] == 'O' && matriz[1, 0] == matriz[1, 1] && matriz[1, 0] == matriz[1, 2])
+            {
+                return ('2');
+            }
+            else if (matriz[2, 0] == 'O' && matriz[2, 0] == matriz[2, 1] && matriz[2, 0] == matriz[2, 2])
+            {
+                return ('2');
+            }
+            else if (matriz[2, 0] == 'O' && matriz[2, 0] == matriz[1, 1] && matriz[2, 0] == matriz[0, 2])
+            {
+                return ('2');
+            }
+            else if (matriz[0, 1] == 'O' && matriz[0, 1] == matriz[1, 1] && matriz[0, 1] == matriz[2, 1])
+            {
+                return ('2');
+            }
+            else if (matriz[0, 2] == 'O' && matriz[0, 2] == matriz[1, 2] && matriz[0, 2] == matriz[2, 2])
+            {
+                return ('2');
+            }
+            else
+            {
+                return ('0');
             }
         }
     }
